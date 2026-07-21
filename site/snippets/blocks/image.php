f06f75111f466be67ec96e77be92cbc117beeb06
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:11:45 +0200

    feat: image block renders dominant color into HTML
    Adding dominant color as custom property accessibel in CSS by injecting it into style in the HTML of images.

---
+++ site/snippets/blocks/image.php
@@ -0,0 +1,36 @@
+<?php
+
+/** @var \Kirby\Cms\Block $block */
+$alt     = $block->alt();
+$caption = $block->caption();
+$crop    = $block->crop()->isTrue();
+$link    = $block->link();
+$ratio   = $block->ratio()->or('auto');
+$src     = null;
+
+if ($block->location() == 'web') {
+		$src = $block->src()->esc();
+} elseif ($image = $block->image()->toFile()) {
+		$alt = $alt->or($image->alt());
+		$src = $image->url();
+		$dominantColor = $image->color();
+}
+
+?>
+<?php if ($src): ?>
+<figure<?= Html::attr(['data-ratio' => $ratio, 'data-crop' => $crop], null, ' ') ?>>
+	<?php if ($link->isNotEmpty()): ?>
+	<a href="<?= Str::esc($link->toUrl()) ?>">
+		<img src="<?= $src ?>" alt="<?= $alt->esc() ?>" style="--dominant-color: <?= $dominantColor ?>">
+	</a>
+	<?php else: ?>
+	<img src="<?= $src ?>" alt="<?= $alt->esc() ?>" style="--dominant-color: <?= $dominantColor ?>">
+	<?php endif ?>
+
+	<?php if ($caption->isNotEmpty()): ?>
+	<figcaption>
+		<?= $caption ?>
+	</figcaption>
+	<?php endif ?>
+</figure>
+<?php endif ?>
