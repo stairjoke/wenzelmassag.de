c49f21eee8f031cf12f81ff301583f2236f20c14
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:11:43 +0200

    feat: structuring page: header, main, and footer

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,38 +2,41 @@
 <html lang="en">
 	<?php snippet('body.head'); ?>
 	<body>
-		<?php
-		/*
-			Display an image respecting light and dark mode, if the field `headlineType` is set to "image"
-		*/
-		if($page->headlineType() == "image"): ?>
-			<picture>
-			<?php $headlineImage = $page->headlineImage()->toFiles();
-			foreach($headlineImage as $image) : ?>
-				<source srcset="<?= $image->url() ?>" media="(prefers-color-scheme: <?= $image->colorscheme() ?>)" />
-			<?php endforeach; ?>
-				<img srcset="<?= $headlineImage->first()->url() ?>" alt="<?= $headlineImage->first()->alt() ?>" />
-			</picture>
-		<?php endif; // headline is image
+		<header></header>
+		<main>
+			<?php
+			/*
+				Display an image respecting light and dark mode, if the field `headlineType` is set to "image"
+			*/
+			if($page->headlineType() == "image"): ?>
+				<picture>
+				<?php $headlineImage = $page->headlineImage()->toFiles();
+				foreach($headlineImage as $image) : ?>
+					<source srcset="<?= $image->url() ?>" media="(prefers-color-scheme: <?= $image->colorscheme() ?>)" />
+				<?php endforeach; ?>
+					<img srcset="<?= $headlineImage->first()->url() ?>" alt="<?= $headlineImage->first()->alt() ?>" />
+				</picture>
+			<?php endif; // headline is image
 
-		/*
-			Render the layout field as a simple layout:
+			/*
+				Render the layout field as a simple layout:
 
-			<section class=grid id=abc>
-				<div class=column style="--span: X">
-					[BLOCKS content]
+				<section class=grid id=abc>
+					<div class=column style="--span: X">
+						[BLOCKS content]
+					</div>
+				</section>
+			*/
+			foreach ($page->layout()->toLayouts() as $layout): ?>
+			<section class="grid" id="<?= $layout->id() ?>">
+				<?php foreach ($layout->columns() as $column): ?>
+				<div class="column" style="--span:<?= $column->span() ?>">
+					<?= $column->blocks() ?>
 				</div>
+				<?php endforeach ?>
 			</section>
-		*/
-		foreach ($page->layout()->toLayouts() as $layout): ?>
-		<section class="grid" id="<?= $layout->id() ?>">
-			<?php foreach ($layout->columns() as $column): ?>
-			<div class="column" style="--span:<?= $column->span() ?>">
-				<?= $column->blocks() ?>
-			</div>
 			<?php endforeach ?>
-		</section>
-		<?php endforeach ?>
-		</div>
+		</main>
+		<footer></footer>
 	</body>
 </html>
