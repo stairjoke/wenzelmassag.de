3ca1e75622f67e693756fc12d7b012b9f4852da6
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:12:07 +0200

    chore: cleaning language file
    Deleting the translations used specifically by entspannt.digital.

--- site/languages/de.php
+++ site/languages/de.php
@@ -2,6 +2,7 @@
 
 return [
 	'code' => 'de',
+	'url' => '/', //Delete when building a multi-langiuage site: https://getkirby.com/docs/guide/languages#language-specific-urls
 	'default' => true,
 	'direction' => 'ltr',
 	'locale' => [
@@ -9,44 +10,6 @@ return [
 	],
 	'name' => 'Deutsch',
 	'translations' => [
-		'callback' => 'Rückruf',
-		'email' => 'E-Mail',
-		'feed' => 'Feed',
-		'footnotes' => 'Fußnoten',
-		'form-block.email-placeholder' => "Damit ich Ihnen antworten kann.",
-		'form-block.error-heading' => "Es gab Fehler beim Senden der Nachricht",
-		'form-block.error-captcha' => "Beim überprüfen des Feldes \"SPAM-Schutz\" wurde ein Fehler gefunden.",
-		'form-block.error-hacking' => "Der Server hat Ihre Anfrage als potentiellen Hackerangriff abgelehnt.",
-		'form-block.error-info' => "Ihre Nachricht wurde nicht versandt, bitte korrigieren Sie die Fehler und versuchen Sie es erneut. Folgende Fehler wurden gefunden:",
-		'form-block.error-validation' => "Ein oder mehrere Felder wurden nicht oder fehlerhaft ausgefüllt: ",
-		'form-block.error-sending' => "Es gab einen Fehler auf dem Server, Ihre Nachricht konnte nicht versand werden. Bitte versuchen Sie es erneut, oder senden Sie eine E-Mail an <a href=\"mailto:wenzel@entspannt.digital\">wenzel@entspannt.digital</a>.",
-		'form-block.error-spam' => "Ihre Nachricht wurde vom Server als SPAM eingestuft. Falls Sie felder von Ihrem browser automatisch ausfüllen lassen haben, versuchen Sie es bitte erneut ohne automatisches Ausfüllen.",
-		'form-block.validation-name' => "keinen oder zu kurzen Namen angegeben",
-		'form-block.validation-email' => "keine gültige E-Mail-Adresse angegeben",
-		'form-block.validation-fon' => "keine gültige Telefonnummer angegeben",
-		'form-block.validation-message' => "Nachricht leer oder zu kurz",
-		'form-block.validation-privacy' => "Datenschutzerklärung nicht akzeptiert",
-		'form-block.fon-placeholder' => "Altmodisch? Mag sein.",
-		'form-block.file-attachment' => "Dateianhang",
-		'form-block.name-placeholder' => "Wie darf ich Sie grüßen?",
-		'form-block.message-placeholder' => "Wie mache ich Sie zum glücklichen Kunde?",
-		'form-block.obligatory' => "Erforderliche Angabe.",
-		'form-block.prefer-reply-via' => 'Präferenz • Rückruf oder E-Mail?',
-		'form-block.privacy-checkbox-label' => 'Ich habe die (link: {URL} text: Datenschutzerklärung target: _blank) gelesen, verstanden und bin einverstanden.*',
-		'form-block.spam-rejected' => "<em>Ihre Nachricht wurde als SPAM abgelehnt!</em> Die wird nicht zugestellt. Falls dies ein Fehler ist, senden Sie mit bitte eine E-Mail direkt und nicht über das Formular.",
-		'form-block.spam-protection' => "SPAM-Schutz",
-		'form-block.spam-challenge' => "Bitte addieren Sie diese beiden Zahlen und tragen Sie das Ergebnis ein",
-		'form-block.spam-placeholder' => "Bitte addieren Sie {one} und {two}",
-		'form-block.submit' => "Absenden",
-		'form-block.success' => "Ihre Nachricht ist unterwegs, vielen Dank!",
-		'image' => 'Bild',
-		'message' => 'Nachricht',
-		'menu-toggle.label' => 'Menü',
-		'menu-toggle.aria' => 'Navigation ein und ausblenden',
-		'menu-toggle.menu-list-label' => 'Menü',
-		'name' => 'Name',
-		'no-preference' => 'Keine Präferenz',
-		'phone' => 'Telefon'
-	],
-	'url' => '/'
+		'example' => 'Beispiel'
+	]
 ];
