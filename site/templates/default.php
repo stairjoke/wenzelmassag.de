6a59b55362513cc9b5a7a4d624c6d934943d826c
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:11:41 +0200

    chore: temporary sample content

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,6 +2,10 @@
 <html lang="en">
 	<?php snippet('body.head'); ?>
 	<body>
-		<?= $page->text()->kt() ?>
+		<h1>Technik und Design einfach für Menschen</h1>
+		<h2>Ich mach das für Sie!</h2>
+		<p>Hier entsteht die neue Webseite von Entspannt.Digital, Digital Design Studio. Ich freue mich über Ihr Interesse!</p>
+		<p>Ab Mai 2025 nehme ich Ihren Auftrag gerne an, <a href="mailto:wenzel@entspannt.digital">senden Sie jetzt eine Anfrage: wenzel@entspannt.digital</a></p>
+		<p><a href="impressum.html">Impressum,</a> <a href="datenschutz.html">Datenschutz.</a></p>
 	</body>
 </html>
