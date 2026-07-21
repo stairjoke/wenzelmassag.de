25ec62710b676edc36296a754e34c67f494a7e79
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:11:59 +0200

    feat+fix: improving figure styling
    - Adding improved support for figcaptions
    - Fixing issue where shadow of one figure would fall on another, darkening surrounding images

--- site/snippets/blocks/image.php
+++ site/snippets/blocks/image.php
@@ -20,19 +20,21 @@ if ($block->location() == 'web') {
 
 ?>
 <?php if ($src): ?>
-<figure<?= Html::attr(['data-ratio' => $ratio, 'data-crop' => $crop], null, ' ') ?>>
-	<?php if ($link->isNotEmpty()): ?>
-	<a href="<?= Str::esc($link->toUrl()) ?>">
-		<img src="<?= $src ?>" alt="<?= $alt->esc() ?>" style="--dominant-color: <?= $dominantColor ?>" class="<?= e($shadow, null, 'noShadow') ?>">
-	</a>
-	<?php else: ?>
-	<img src="<?= $src ?>" alt="<?= $alt->esc() ?>" style="--dominant-color: <?= $dominantColor ?>" class="<?= e($shadow, null, 'noShadow') ?>">
-	<?php endif ?>
+	<div class="image-container">
+		<figure<?= Html::attr(['data-ratio' => $ratio, 'data-crop' => $crop], null, ' ') ?>>
+			<?php if ($link->isNotEmpty()): ?>
+			<a href="<?= Str::esc($link->toUrl()) ?>">
+				<img src="<?= $src ?>" alt="<?= $alt->esc() ?>" style="--dominant-color: <?= $dominantColor ?>" class="<?= e($shadow, null, 'noShadow') ?>">
+			</a>
+			<?php else: ?>
+			<img src="<?= $src ?>" alt="<?= $alt->esc() ?>" style="--dominant-color: <?= $dominantColor ?>" class="<?= e($shadow, null, 'noShadow') ?>">
+			<?php endif ?>
 
-	<?php if ($caption->isNotEmpty()): ?>
-	<figcaption inert>
-		<p><?= $caption ?></p>
-	</figcaption>
-	<?php endif ?>
-</figure>
+			<?php if ($caption->isNotEmpty()): ?>
+			<figcaption inert>
+				<p><?= $caption ?></p>
+			</figcaption>
+			<?php endif ?>
+		</figure>
+	</div>
 <?php endif ?>
