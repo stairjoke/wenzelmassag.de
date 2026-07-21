e232dbdd7dd5d6970686f8f7def99d8970c955cf
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:12:01 +0200

    feat: improving accessibility

--- site/snippets/blocks/responsive-card.php
+++ site/snippets/blocks/responsive-card.php
@@ -4,14 +4,29 @@
 	<?php endif; ?>
 
 	<?php if($image = $block->image()->toFile()) :
-		$sizes = "(max-width: 38rem) calc(100vw - 2rem), calc(50vw - 6rem)";
+		$sizes = "(width < 38rem) calc(100vw - 2rem), (width >= 38rem) calc(50vw - 6rem)";
 		$shadow = ($image->shadow()->isNotEmpty()) ? $image->shadow()->toBool() : true;
+
+		$alt           = $image->alt();
+		$caption       = $image->caption();
+		$dominantColor = $image->color();
+		$shadow        = ($image->shadow()->isNotEmpty()) ? $image->shadow()->toBool() : true;
 	?>
-	<picture <?= e($image->color()->isNotEmpty(), 'style="--dominant-color:'.$image->color().'"'); ?>>
-		<source srcset="<?= $image->srcset('column-avif') ?>" sizes="<?= $sizes ?>" type="image/avif" />
-		<source srcset="<?= $image->srcset('scolumn-webp') ?>" sizes="<?= $sizes ?>" type="image/webp" />
-		<img alt="<?= $image->alt() ?>" src="<?= $image->resize(264)->url()?>" srcset="<?= $image->srcset('column') ?>" sizes="<?= $sizes ?>" <?= e($shadow, 'class="shadow"') ?> width="264" height="<?= $image->resize(264)->height() ?>"/>
-	</picture>
+	<div class="image-container <?= e($shadow, 'shadow') ?>" style="--dominant-color: <?= $dominantColor ?>" class="<?= e($shadow, null, 'noShadow') ?>">
+		<figure>
+			<?php $sizes = "(max-width: 38rem) calc(100vw - 2rem), calc(100vw - 6rem)"; ?>
+			<picture>
+				<source srcset="<?= $image->srcset('column-avif') ?>" sizes="<?= $sizes ?>" type="image/avif" />
+				<source srcset="<?= $image->srcset('scolumn-webp') ?>" sizes="<?= $sizes ?>" type="image/webp" />
+				<img alt="<?= $image->alt() ?>" src="<?= $image->resize(576)->url()?>" srcset="<?= $image->srcset('column') ?>" sizes="<?= $sizes ?>" width="576" height="<?= $image->resize(576)->height() ?>"/>
+			</picture>
+			<?php if ($caption->isNotEmpty()): ?>
+				<figcaption inert>
+					<p><?= $caption ?></p>
+				</figcaption>
+			<?php endif ?>
+		</figure>
+	</div>
 	<?php endif; ?>
 
 	<?php if($text  = $block->text()) : ?>
