d9adcf7df6fb2d083cb19e7decf4878bd62bc90b
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:11:49 +0200

    feat: form renders to HTML

--- site/snippets/blocks/form.php
+++ site/snippets/blocks/form.php
@@ -14,20 +14,20 @@
 <form class="form-block">
 	<?php if($name) : ?>
 	<div class="input">
-		<label for="name"><?= t('name', 'Name') ?></label>
-		<input id="name" name="name" type="text" placeholder="<?= t('form-block.name-placeholder', 'How may I call you?') ?>" />
+		<label for="name"><?= t('name', 'Name') ?>*</label>
+		<input id="name" name="name" type="text" placeholder="<?= t('form-block.name-placeholder', 'How may I call you?') ?>" required />
 	</div>
 	<?php endif;
 	if($email): ?>
 	<div class="input">
-		<label for="email"><?= t('email', 'Email') ?></label>
-		<input id="email" name="email" type="email" placeholder="<?= t('form-block.email-placeholder', "So I can reply to you.") ?>" />
+		<label for="email"><?= t('email', 'Email') ?>*</label>
+		<input id="email" name="email" type="email" placeholder="<?= t('form-block.email-placeholder', "So I can reply to you.") ?>" required />
 	</div>
 	<?php endif;
 	if($fon): ?>
 	<div class="input">
-		<label for="fon"><?= t('phone', 'Phone') ?></label>
-		<input id="fon" name="fon" type="tel" placeholder="<?= t('form-block.fon-placeholder', "Old fashioned? Maybe.") ?>" />
+		<label for="fon"><?= t('phone', 'Phone') ?>*</label>
+		<input id="fon" name="fon" type="tel" placeholder="<?= t('form-block.fon-placeholder', "Old fashioned? Maybe.") ?>" required />
 	</div>
 	<?php endif;
 	if($replyVia): ?>
@@ -40,8 +40,8 @@
 	<?php endif;
 	if($message): ?>
 	<div class="input big">
-		<label for="message"><?= t('message', 'Message') ?></label>
-		<textarea id="message" name="message" placeholder="<?= t('form-block.message-placeholder', "What should I do to make you a happy customer?") ?>"></textarea>
+		<label for="message"><?= t('message', 'Message') ?>*</label>
+		<textarea id="message" name="message" placeholder="<?= t('form-block.message-placeholder', "How do I make you a happy customer?") ?>" required></textarea>
 	</div>
 	<?php endif;
 	if($files): ?>
@@ -52,11 +52,12 @@
 	<?php endif;
 	if($captcha): ?>
 	<div class="input big">
-		<label for="captcha"><?= t('form-block.spam-protection', 'SPAM protection') ?> • <?= t('form-block.spam-challenge', "Please add these two numbers and enter the result") ?>: <?php $one = rand(1, 5); $two = rand(1, 5); echo($one . ", " . $two) ?></label>
-		<input id="captcha" name="captcha" type="number" />
+		<label for="captcha"><?= t('form-block.spam-protection', 'SPAM protection') ?>* • <?= t('form-block.spam-challenge', "Please add these two numbers and enter the result") ?>: <?php $one = rand(1, 5); $two = rand(1, 5); echo($one . ", " . $two) ?></label>
+		<input id="captcha" name="captcha" type="number" placeholder="<?= I18n::template('form-block.spam-placeholder', "Please add " . $one . " and " . $two, ['one' => $one, 'two' => $two]) ?>" required />
 	</div>
 	<?php endif ?>
 
 	<input name="key" type="hidden" value="<?= hash('md5', $one + $two) ?>" />
 	<input type="submit" value="<?= t('form-block.submit', 'send') ?>" />
+	<p>* <?= t('form-block.obligatory', "Obligatory field.") ?></p>
 </form>
