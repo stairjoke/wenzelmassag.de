2418d3c7eaa8f815ca4ce47582af4e873e07888b
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:12:11 +0200

    design: changing HTML title
    Now using the format "title (site name)" to improve appearance of pages in search engines.

--- site/snippets/html.head.php
+++ site/snippets/html.head.php
@@ -1,4 +1,4 @@
-<title><?= $site->title() ?> &gt; <?= $page->title() ?></title>
+<title><?= $page->title() ?> (<?= $site->title() ?>)</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <?php if($site->favicoDefault()->isNotEmpty()): ?>
