ad6b9ca1cce405a6b7c5d2a4378f1ac56301c5ae
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:11:45 +0200

    feat: rendering simple nav with listed pages

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,7 +2,20 @@
 <html lang="en">
 	<?php snippet('html.head'); ?>
 	<body>
-		<header></header>
+		<header>
+			<nav>
+				<ol>
+					<li>
+						<a class="logotype" href="<?= $site->url() ?>"><span>Entspannt.Digital</span> <span>Digital Design Studio</span></a>
+					</li>
+					<?php foreach($site->children()->listed() as $item): ?>
+					<li>
+						<a href="<?= $item->url() ?>" <?php e($item->isOpen(), 'class=current') ?>><?= $item->title() ?></a>
+					</li>
+					<?php endforeach; ?>
+				</ol>
+			</nav>
+		</header>
 		<main>
 			<?php
 			/*
