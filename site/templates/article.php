6adc9126c6bc796871b37f12705916927960361a
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 15:59:33 +0200

    Bringing public and repo in sync

--- site/templates/article.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-	<?php snippet('body.head'); ?>
-	<body>
-		Article
-		<?= $page->text()->kt() ?>
-	</body>
-</html>
--- site/templates/default.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-	<?php snippet('body.head'); ?>
-	<body>
-		<?= $page->text()->kt() ?>
-	</body>
-</html>
