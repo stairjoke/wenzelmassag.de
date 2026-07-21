f54bd9084534359da0a98758792d08a375da3b84
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:11:41 +0200

    feat: improved typographic rhythm, added h4-h6
    All block-level elements have a `margin-block: var(--line-height)` and will set the `margin-block-start` of their first, and the `margin-block-end` of their last child to zero. — To ensure typographic rhythm is maintained in layouts with multiple columns, do not use `H1` and `H2` elements inside columns. `H3`, `H4`, `H5` and `H6`-elements may be used within columns. `H1` and `H2` elements will grow in height beyond normal text and break vertical rhythm.

--- site/templates/default.php
+++ site/templates/default.php
@@ -4,9 +4,14 @@
 	<body>
 		<h1>Technik und Design einfach für Menschen</h1>
 		<h2>Ich mach das für Sie!</h2>
-		<h3>Überschrift dritter Ordnung</h3>
-		<p>Hier entsteht die neue Webseite von Entspannt.Digital, Digital Design Studio. Ich freue mich über Ihr Interesse!</p>
-		<p>Ab Mai 2025 nehme ich Ihren Auftrag gerne an, <a href="mailto:wenzel@entspannt.digital">senden Sie jetzt eine Anfrage: wenzel@entspannt.digital</a></p>
-		<p><a href="impressum.html">Impressum,</a> <a href="datenschutz.html">Datenschutz.</a></p>
+		<div class="columns">
+			<h3>Lange Überschrift dritter Ordnung Lange Überschrift dritter Ordnung Lange Überschrift dritter Ordnung</h3>
+			<h4>Überschrift vierter Ordnung</h4>
+			<h5>Überschrift fünfter Ordnung</h5>
+			<h6>Überschrift sechster Ordnung</h6>
+			<p>Hier entsteht die neue Webseite von Entspannt.Digital, Digital Design Studio. Ich freue mich über Ihr Interesse!</p>
+			<p>Ab Mai 2025 nehme ich Ihren Auftrag gerne an, <a href="mailto:wenzel@entspannt.digital">senden Sie jetzt eine Anfrage: wenzel@entspannt.digital</a></p>
+			<p><a href="impressum.html">Impressum,</a> <a href="datenschutz.html">Datenschutz.</a></p>
+		</div>
 	</body>
 </html>
