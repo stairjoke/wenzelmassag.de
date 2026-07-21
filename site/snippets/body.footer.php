6383ec978e8f35790a9e44019510e2c8bb46fb45
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:11:55 +0200

    feat: rendering page footer

--- site/snippets/body.footer.php
+++ site/snippets/body.footer.php
@@ -1,1 +1,10 @@
-<footer></footer>
+<footer>
+	<?php
+		$links = $site->links()->toStructure();
+		foreach($links as $link):
+			$rel = ($link->rel()->isNotEmpty()) ? $link->rel() : false;
+			?>
+			<a<?= e($rel, ' rel="' . $rel . '"') ?> href="<?= $link->href()->toUrl() ?>"><?= $link->text() ?></a>
+		<?php endforeach;
+	?>
+</footer>
