54b39988ff8551b274dc4f8d2ca1731b7784713e
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:12:01 +0200

    fix: responsive card image size

--- site/snippets/blocks/responsive-card.php
+++ site/snippets/blocks/responsive-card.php
@@ -10,7 +10,7 @@
 	<picture <?= e($image->color()->isNotEmpty(), 'style="--dominant-color:'.$image->color().'"'); ?>>
 		<source srcset="<?= $image->srcset('column-avif') ?>" sizes="<?= $sizes ?>" type="image/avif" />
 		<source srcset="<?= $image->srcset('scolumn-webp') ?>" sizes="<?= $sizes ?>" type="image/webp" />
-		<img alt="<?= $image->alt() ?>" src="<?= $image->resize(264)->url()?>" srcset="<?= $image->srcset('column') ?>" sizes="<?= $sizes ?>" <?= e($shadow, 'class="shadow"') ?> width="264" height="264"/>
+		<img alt="<?= $image->alt() ?>" src="<?= $image->resize(264)->url()?>" srcset="<?= $image->srcset('column') ?>" sizes="<?= $sizes ?>" <?= e($shadow, 'class="shadow"') ?> width="264" height="<?= $image->resize(264)->height() ?>"/>
 	</picture>
 	<?php endif; ?>
 
