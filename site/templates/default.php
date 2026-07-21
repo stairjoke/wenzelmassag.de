9987e2b0e5e26ec71b7bbf6fec3f57061ec41565
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:11:41 +0200

    feat: using dynamic font sizes and line heights
    Based on viewport width, the line-height and font-size variables adapt within clamped values.

--- site/templates/default.php
+++ site/templates/default.php
@@ -4,6 +4,7 @@
 	<body>
 		<h1>Technik und Design einfach für Menschen</h1>
 		<h2>Ich mach das für Sie!</h2>
+		<h3>Überschrift dritter Ordnung</h3>
 		<p>Hier entsteht die neue Webseite von Entspannt.Digital, Digital Design Studio. Ich freue mich über Ihr Interesse!</p>
 		<p>Ab Mai 2025 nehme ich Ihren Auftrag gerne an, <a href="mailto:wenzel@entspannt.digital">senden Sie jetzt eine Anfrage: wenzel@entspannt.digital</a></p>
 		<p><a href="impressum.html">Impressum,</a> <a href="datenschutz.html">Datenschutz.</a></p>
