851c2c2aed4d90041e9aacc7b11781d56b1f3d24
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:11:45 +0200

    feat: adding aria-current to nav

--- site/templates/default.php
+++ site/templates/default.php
@@ -6,11 +6,11 @@
 			<nav>
 				<ol>
 					<li>
-						<a class="logotype" href="<?= $site->url() ?>"><span>Entspannt.Digital</span> <span>Digital Design Studio</span></a>
+						<a class="logotype" href="<?= $site->url() ?>" <?php e($page->isOpen(), 'aria-current=page') ?>><span>Entspannt.Digital</span> <span>Digital Design Studio</span></a>
 					</li>
 					<?php foreach($site->children()->listed() as $item): ?>
 					<li>
-						<a href="<?= $item->url() ?>" <?php e($item->isOpen(), 'class=current') ?>><?= $item->title() ?></a>
+						<a href="<?= $item->url() ?>" <?php e($item->isOpen(), 'aria-current=page') ?>><?= $item->title() ?></a>
 					</li>
 					<?php endforeach; ?>
 				</ol>
