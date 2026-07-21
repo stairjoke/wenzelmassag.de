494a19c4afda1573fdd71dc480e084c2364ed513
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:11:51 +0200

    feat: improved translation

--- site/languages/de.php
+++ site/languages/de.php
@@ -21,7 +21,7 @@ return [
 		'form-block.obligatory' => "Erforderliche Angabe.",
 		'form-block.prefer-reply-via' => 'Präferenz • Rückruf oder E-Mail?',
 		'form-block.privacy-label' => 'Datenschutz-Einwilligung',
-		'form-block.spam-rejected' => "<em>Ihre Nachricht wurde als SPAM abgelehnt!</em> Die wird nicht zugestellt. Falls dies ein Fehler ist, senden Sie mit bitte eine E-Mail direkt, nicht über das Formular.",
+		'form-block.spam-rejected' => "<em>Ihre Nachricht wurde als SPAM abgelehnt!</em> Die wird nicht zugestellt. Falls dies ein Fehler ist, senden Sie mit bitte eine E-Mail direkt und nicht über das Formular.",
 		'form-block.spam-protection' => "SPAM-Schutz",
 		'form-block.spam-challenge' => "Bitte addieren Sie diese beiden Zahlen und tragen Sie das Ergebnis ein",
 		'form-block.spam-placeholder' => "Bitte addieren Sie {one} und {two}",
