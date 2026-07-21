b8a499f1b0fa54582cc6e5e043c72986f8a92844
Wenzel Massag (@stairjoke) <mail@wenzelmassag.de>
2026-07-21 16:11:43 +0200

    feat: rendering content to HTML
    Rendering a headline-image, if there is one, followed by the content defined in the layout field.

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,16 +2,38 @@
 <html lang="en">
 	<?php snippet('body.head'); ?>
 	<body>
-		<h1>Technik und Design einfach für Menschen</h1>
-		<h2>Ich mach das für Sie!</h2>
-		<div class="columns">
-			<h3>Lange Überschrift dritter Ordnung Lange Überschrift dritter Ordnung Lange Überschrift dritter Ordnung</h3>
-			<h4>Überschrift vierter Ordnung</h4>
-			<h5>Überschrift fünfter Ordnung</h5>
-			<h6>Überschrift sechster Ordnung</h6>
-			<p>Hier entsteht die neue Webseite von Entspannt.Digital, Digital Design Studio. Ich freue mich über Ihr Interesse!</p>
-			<p>Ab Mai 2025 nehme ich Ihren Auftrag gerne an, <a href="mailto:wenzel@entspannt.digital">senden Sie jetzt eine Anfrage: wenzel@entspannt.digital</a></p>
-			<p><a href="impressum.html">Impressum,</a> <a href="datenschutz.html">Datenschutz.</a></p>
+		<?php
+		/*
+			Display an image respecting light and dark mode, if the field `headlineType` is set to "image"
+		*/
+		if($page->headlineType() == "image"): ?>
+			<picture>
+			<?php $headlineImage = $page->headlineImage()->toFiles();
+			foreach($headlineImage as $image) : ?>
+				<source srcset="<?= $image->url() ?>" media="(prefers-color-scheme: <?= $image->colorscheme() ?>)" />
+			<?php endforeach; ?>
+				<img srcset="<?= $headlineImage->first()->url() ?>" alt="<?= $headlineImage->first()->alt() ?>" />
+			</picture>
+		<?php endif; // headline is image
+
+		/*
+			Render the layout field as a simple layout:
+
+			<section class=grid id=abc>
+				<div class=column style="--span: X">
+					[BLOCKS content]
+				</div>
+			</section>
+		*/
+		foreach ($page->layout()->toLayouts() as $layout): ?>
+		<section class="grid" id="<?= $layout->id() ?>">
+			<?php foreach ($layout->columns() as $column): ?>
+			<div class="column" style="--span:<?= $column->span() ?>">
+				<?= $column->blocks() ?>
+			</div>
+			<?php endforeach ?>
+		</section>
+		<?php endforeach ?>
 		</div>
 	</body>
 </html>
